63a86fa1829e1d72499f11b7ac5323b4d03fb094
ngocngo <ngothingocbk99@gmail.com>
2022-11-09 09:05:01 +0700

    Update url to get categories

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,28 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
-const indexRouter = require('./src/routes/index');
-const authRouter = require('./src/routes/auth');
+const indexRouter = require("./src/routes/index");
+const authRouter = require("./src/routes/auth");
 // var usersRouter = require('./src/routes/users');
 
 var app = express();
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://127.0.0.1:5173",
 };
 app.use(cors(corsOptions));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 const db = require("./src/models");
-db.sequelize.sync()
+db.sequelize
+  .sync()
   .then(() => {
     console.log("Synced db.");
   })
@@ -30,21 +30,21 @@ db.sequelize.sync()
     console.log("Failed to sync db: " + err.message);
   });
 
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.error(err)
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  console.error(err);
 
   if (err.statusCode === 500) {
     res.status(err.statusCode || 500);
@@ -53,7 +53,6 @@ app.use(function(err, req, res, next) {
     res.status(err.statusCode);
     res.json({ error: err.message });
   }
-  
 });
 
 module.exports = app;
