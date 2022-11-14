ab575728f6f9bdfe43e1d1b288301c715f4a7f36
ngocngo <ngothingocbk99@gmail.com>
2022-11-14 09:13:52 +0700

    fe - create vaccine detail reducer

--- frontend/src/reducers/vaccine.reducer.js
+++ frontend/src/reducers/vaccine.reducer.js
@@ -4,6 +4,12 @@ import {
   VACCINE_LIST_FAIL
 } from '../constants/vaccine.constant';
 
+import {
+  VACCINE_DETAIL_REQUEST,
+  VACCINE_DETAIL_SUCCESS,
+  VACCINE_DETAIL_FAIL
+} from '../constants/vaccine.constant';
+
 const initialState = {
   loading: false,
   vaccines: [],
@@ -22,7 +28,24 @@ export const vaccineListReducer = (state = initialState, action) => {
         totalItem: action.payload.count
       };
     case VACCINE_LIST_FAIL:
-      return { ...state, loading: false, error: action.payload.error };
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const vaccineDetailReducer = (state = {}, action) => {
+  switch (action.type) {
+    case VACCINE_DETAIL_REQUEST:
+      return { ...state, loading: true };
+    case VACCINE_DETAIL_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        vaccine: action.payload
+      };
+    case VACCINE_DETAIL_FAIL:
+      return { ...state, loading: false, error: action.payload };
     default:
       return state;
   }
