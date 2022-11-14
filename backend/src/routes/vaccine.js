bd0e97f5e22ca911369f45efec7d3c3c2121c966
ngothingoc10 <ngothingobk99@gmail.com>
2022-11-14 23:26:35 +0700

    be - implement admin roles create vaccine

--- backend/src/routes/vaccine.js
+++ backend/src/routes/vaccine.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { authMiddleware } = require('../middlewares/index');
 const { VaccineController } = require('../controllers');
 
 /* GET users listing. */
-router.post('/', VaccineController.create);
+router.post('/', authMiddleware.validateToken, authMiddleware.isAdmin, VaccineController.create);
 router.get('/', VaccineController.find);
 router.get('/:id', VaccineController.findOne);
 router.put('/:id', VaccineController.update);
