38ef9d16577e062e3c2216d17c941f528ff1a4ff
ngocngo <ngothingocbk99@gmail.com>
2022-12-17 20:45:33 +0700

    fe - add token for api get schedule configs

--- frontend/src/actions/schedule_config.action.js
+++ frontend/src/actions/schedule_config.action.js
@@ -13,15 +13,26 @@ import {
 import { logout } from './user.action';
 import { BASE_URL } from '../constants/base_url.constant';
 
-export const getScheduleConfigs = () => async (dispatch) => {
+export const getScheduleConfigs = () => async (dispatch, getState) => {
   try {
     dispatch({
       type: SCHEDULE_CONFIGS_REQUEST
     });
 
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        'Content-Type': 'application/json'
+      }
+    };
+
     const url = `${BASE_URL}/api/schedule-configs`;
 
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url, config);
 
     dispatch({
       type: SCHEDULE_CONFIGS_SUCCESS,
