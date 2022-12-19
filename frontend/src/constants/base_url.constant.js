398ccac48debdac0a9a0728a1e5457d2d1324c2f
ngocngo <ngothingocbk99@gmail.com>
2022-12-19 16:54:29 +0700

    fe - update base url

--- frontend/src/constants/base_url.constant.js
+++ frontend/src/constants/base_url.constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8080';
-// export const BASE_URL = 'http://datn-vaccine-center.website:3001';
+// export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://datn-vaccine-center.website:3001';
